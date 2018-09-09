02f4fede23be8847eea92e7568cde050a8579970
linus123 <linus_pagan@hotmail.com>
2018-09-09 07:16:15 -0500

    First commit

---
+++ src/Core/Unit01/PeakFinding1DTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Core.Unit01
+{
+    public class PeakFinding1DTests
+    {
+        [Fact]
+        public void ShouldNotFindPeakGivenNoElements()
+        {
+            var peakFinding1D = new PeakFinding1D();
+
+            var peek = peakFinding1D.FindPeek(
+                new int[0]);
+
+            peek.Should().NotHaveValue();
+        }
+
+        [Fact]
+        public void ShouldReturnNumberGivenSingleItmeInArray()
+        {
+            var peakFinding1D = new PeakFinding1D();
+
+            var peek = peakFinding1D.FindPeek(
+                new int[] { 1 });
+
+            peek.Should().HaveValue();
+            peek.Should().Be(1);
+        }
+
+    }
+}
---
+++ src/Core/Unit01/PeakFinding1d.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Core.Unit01
+{
+    public class PeakFinding1D
+    {
+        public int? FindPeek(
+            int[] numbers)
+        {
+            if (!numbers.Any())
+                return null;
+
+            return numbers[0];
+        }
+    }
+}
