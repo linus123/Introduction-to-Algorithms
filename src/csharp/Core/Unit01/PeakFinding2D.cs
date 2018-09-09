1b35f267ec4ece3d82918ba9ebbe53027244925b
linus123 <linus_pagan@hotmail.com>
2018-09-09 13:49:34 -0500

    Brute force working?

--- src/csharp/Core/Unit01/PeakFinding2D.cs
+++ src/csharp/Core/Unit01/PeakFinding2D.cs
@@ -17,29 +17,125 @@
 
         public int FindPeek(int[,] grid)
         {
-            if (grid.GetLength(0) == 1
-                && grid.GetLength(1) == 1)
-                return grid[0, 0];
+            var grd = new Grid(grid);
 
-            if (grid.GetLength(0) == 2
-                && grid.GetLength(1) == 1)
+            if (grd.Width <= 2
+                && grd.Height <= 2)
             {
-                if (grid[0, 0] >= grid[1, 0])
-                    return grid[0, 0];
-
-                return grid[1, 0];
+                return grd.GetMaxValue();
             }
 
-            if (grid.GetLength(0) == 1
-                && grid.GetLength(1) == 2)
+            for (int y = 0; y < grd.Height; y++)
             {
-                if (grid[0, 0] >= grid[0, 1])
-                    return grid[0, 0];
-
-                return grid[0, 1];
+                for (int x = 0; x < grd.Width; x++)
+                {
+                    if (grd.IsPeak(x, y))
+                        return grd.GetValue(x, y);
+                }
             }
 
             return grid[0, 0];
         }
+
+        private class Grid
+        {
+            private readonly int[,] _grid;
+
+            public Grid(
+                int[,] grid)
+            {
+                _grid = grid;
+            }
+
+            public int Height
+            {
+                get { return _grid.GetLength(0); }
+            }
+
+            public int Width
+            {
+                get { return _grid.GetLength(1); }
+            }
+
+            public int GetMaxValue()
+            {
+                int max = int.MinValue;
+
+                for (int x = 0; x < _grid.GetLength(1); x++)
+                {
+                    for (int y = 0; y < _grid.GetLength(0); y++)
+                    {
+                        if (_grid[y, x] > max)
+                            max = _grid[y, x];
+                    }
+                }
+
+                return max;
+
+            }
+
+            public int GetValue(int x, int y)
+            {
+                return _grid[y, x];
+            }
+
+            public int GetAboveValue(int x, int y)
+            {
+                var adjustedY = y - 1;
+
+                if (adjustedY < 0)
+                {
+                    return GetValue(x, y);
+                }
+
+                return _grid[adjustedY, x];
+            }
+
+            public int GetBelowValue(int x, int y)
+            {
+                var adjustedY = y + 1;
+
+                if (adjustedY > Height)
+                {
+                    return GetValue(x, y);
+                }
+
+                return _grid[adjustedY, x];
+            }
+
+            public int GetLeftValue(int x, int y)
+            {
+                var adjustedX = x - 1;
+
+                if (adjustedX < 0)
+                {
+                    return GetValue(x, y);
+                }
+
+                return _grid[y, adjustedX];
+            }
+
+            public int GetRightValue(int x, int y)
+            {
+                var adjustedX = x + 1;
+
+                if (adjustedX > Width)
+                {
+                    return GetValue(x, y);
+                }
+
+                return _grid[y, adjustedX];
+            }
+
+            public bool IsPeak(
+                int x, int y)
+            {
+                return GetValue(x, y) >= GetAboveValue(x, y)
+                       && GetValue(x, y) >= GetBelowValue(x, y)
+                       && GetValue(x, y) >= GetLeftValue(x, y)
+                       && GetValue(x, y) >= GetRightValue(x, y);
+            }
+        }
+
     }
 }
--- src/csharp/Core/Unit01/PeakFinding2DTests.cs
+++ src/csharp/Core/Unit01/PeakFinding2DTests.cs
@@ -116,5 +116,76 @@ namespace Core.Unit01
             peak.Should().Be(val);
         }
 
+        [Theory(DisplayName = "Should return only value given 3 x 3 grid with all the same values.")]
+        [InlineData(PeakFinding2D.AlgoType.BruteForce)]
+        public void Test012(
+            PeakFinding2D.AlgoType algoType)
+        {
+            var random = new Random();
+            var val = random.Next();
+
+            var peakFinding2D = new PeakFinding2D(algoType);
+
+            var peak = peakFinding2D.FindPeek(new int[,]
+            {
+                { val, val, val },
+                { val, val, val },
+                { val, val, val }
+            });
+
+            peak.Should().Be(val);
+        }
+
+        [Theory(DisplayName = "Should find peak when in center of 3 x 3 grid.")]
+        [InlineData(PeakFinding2D.AlgoType.BruteForce)]
+        public void Test013(
+            PeakFinding2D.AlgoType algoType)
+        {
+            var peakFinding2D = new PeakFinding2D(algoType);
+
+            var peak = peakFinding2D.FindPeek(new int[,]
+            {
+                { 0, 1, 0 },
+                { 1, 2, 1 },
+                { 0, 1, 0 }
+            });
+
+            peak.Should().Be(2);
+        }
+
+        [Theory(DisplayName = "Should find peak when in x = 0, y = 0 of 3 x 3 grid.")]
+        [InlineData(PeakFinding2D.AlgoType.BruteForce)]
+        public void Test014(
+            PeakFinding2D.AlgoType algoType)
+        {
+            var peakFinding2D = new PeakFinding2D(algoType);
+
+            var peak = peakFinding2D.FindPeek(new int[,]
+            {
+                { 4, 3, 1 },
+                { 3, 2, 1 },
+                { 1, 1, 0 }
+            });
+
+            peak.Should().Be(4);
+        }
+
+        [Theory(DisplayName = "Should find peak when in x = 1, y = 0 of 3 x 3 grid.")]
+        [InlineData(PeakFinding2D.AlgoType.BruteForce)]
+        public void Test015(
+            PeakFinding2D.AlgoType algoType)
+        {
+            var peakFinding2D = new PeakFinding2D(algoType);
+
+            var peak = peakFinding2D.FindPeek(new int[,]
+            {
+                { 3, 4, 1 },
+                { 3, 2, 1 },
+                { 1, 1, 0 }
+            });
+
+            peak.Should().Be(4);
+        }
+
     }
 }
