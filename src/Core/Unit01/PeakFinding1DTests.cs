e2e97ff0f67dda8859cee264842f3c94097ff97d
linus123 <linus_pagan@hotmail.com>
2018-09-09 07:27:02 -0500

    Basic peek working

--- src/Core/Unit01/PeakFinding1DTests.cs
+++ src/Core/Unit01/PeakFinding1DTests.cs
@@ -28,5 +28,48 @@ namespace Core.Unit01
             peek.Should().Be(1);
         }
 
+        [Fact]
+        public void ShouldReturnLargerNumberGivenTwoLenghtArray()
+        {
+            var peakFinding1D = new PeakFinding1D();
+
+            int? peek;
+
+            peek = peakFinding1D.FindPeek(
+                new int[] { 1, 2 });
+
+            peek.Should().HaveValue();
+            peek.Should().Be(2);
+
+            peek = peakFinding1D.FindPeek(
+                new int[] { 2, 1 });
+
+            peek.Should().HaveValue();
+            peek.Should().Be(2);
+        }
+
+        [Fact]
+        public void ShouldReturnNumberGivenTwoLengthArrayWithSameValues()
+        {
+            var peakFinding1D = new PeakFinding1D();
+
+            var peek = peakFinding1D.FindPeek(
+                new int[] { 2, 2 });
+
+            peek.Should().HaveValue();
+            peek.Should().Be(2);
+        }
+
+        [Fact]
+        public void ShouldFindPeekWhenMiddleIsLargestGiven3LengthArray()
+        {
+            var peakFinding1D = new PeakFinding1D();
+
+            var peek = peakFinding1D.FindPeek(
+                new int[] { 1, 2, 1 });
+
+            peek.Should().HaveValue();
+            peek.Should().Be(2);
+        }
     }
 }
--- src/Core/Unit01/PeakFinding1d.cs
+++ src/Core/Unit01/PeakFinding1d.cs
@@ -1,16 +1,30 @@
-﻿using System.Linq;
-
-namespace Core.Unit01
+﻿namespace Core.Unit01
 {
     public class PeakFinding1D
     {
         public int? FindPeek(
             int[] numbers)
         {
-            if (!numbers.Any())
-                return null;
+            if (numbers.Length == 1)
+                return numbers[0];
 
-            return numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (numbers[0] >= numbers[1])
+                        return numbers[0];
+                }
+                else if (i == (numbers.Length - 1))
+                {
+                    if (numbers[i] >= numbers[i - 1])
+                        return numbers[i];
+                }
+                else if (numbers[i] >= numbers[i - 1] && numbers[i] >= numbers[i + 1])
+                    return numbers[i];
+            }
+
+            return null;
         }
     }
 }
